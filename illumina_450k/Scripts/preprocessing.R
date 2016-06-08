357b9bfab58525a987a403eff78deb0dd5127ad7
Randner <randner@mm.st>
2016-06-08 11:35:54 +0200

    added the filtering for samples before and after preprocessing

--- illumina_450k/Scripts/preprocessing.R
+++ illumina_450k/Scripts/preprocessing.R
@@ -1,8 +1,9 @@
 loaded_data <- readRDS(defaults$loaded_dataset_path)
 
 normalized_dataset <- loaded_data %>>=%
-       defaults$filter_samples_default %>>=%
+       defaults$preprocessing_filter_samples_default %>>=%
        defaults$filter_probes_default  %>>=%
-       defaults$normalize_default
+       defaults$normalize_default %>>=%
+       defaults$filter_samples_default
 
 saveRDS(normalized_dataset, file=defaults$preprocessed_dataset_path)
